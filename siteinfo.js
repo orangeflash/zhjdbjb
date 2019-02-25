240cd432500f5a27674f98e32b5ccfa712eb7ee5
orangeflash <1767652421@qq.com>
2019-02-25 14:25:15 +0800

    初始化

---
+++ siteinfo.js
@@ -0,0 +1,12 @@
+var siteInfo = {
+  'title': '',
+  'uniacid': '12',
+  'acid': '12',
+  'multiid': '0',
+  'version': '1.0',
+    'siteroot': 'https://zfb.zhycms.com/app/index.php',
+  'design_method': '3',
+  'redirect_module': '',
+  'template': ''
+  }
+module.exports = siteInfo
